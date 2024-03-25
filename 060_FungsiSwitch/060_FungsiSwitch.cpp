eb90ceef0e15eac658914b54a50df6f2363d35e7
maskikuy <kproject305@gmail.com>
2024-03-25 15:03:44 +0700

    input pilihan

--- 060_FungsiSwitch/060_FungsiSwitch.cpp
+++ 060_FungsiSwitch/060_FungsiSwitch.cpp
@@ -29,3 +29,23 @@ float pengurangan(float a, float b)
 {
     return a - b;
 }
+
+int main()
+{
+    int pilihan;
+
+    do
+    {
+        system("cls");
+        cout << "===========================" << endl;
+        cout << "MENU" << endl;
+        cout << "1. Penjumlahan " << endl;
+        cout << "2. Pembagian " << endl;
+        cout << "3. Perkalian " << endl;
+        cout << "4. Pengurangan " << endl;
+        cout << "5. Exit " << endl;
+        cout << "Masukan pilihan 1/2/3/4/5/6 = ";
+        cin >> pilihan;
+
+       
+    }
